c53d61d40a32dbd97b9c051f4795efa2601f9060
PoLLi <1657076+poLLi@users.noreply.github.com>
2026-02-01 13:02:36 +0100

    Renames project to EmberArcane
    Updates the project's group ID, description, author, main entry point, and root project name to reflect the new EmberArcane project.

--- src/main/java/org/polli/emberarcane/EmberArcane.java
+++ src/main/java/org/polli/emberarcane/EmberArcane.java
@@ -1,13 +1,13 @@
-package com.example.exampleplugin;
+package org.polli.emberarcane;
 
 import com.hypixel.hytale.logger.HytaleLogger;
 import com.hypixel.hytale.server.core.plugin.JavaPlugin;
 import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
 
-public class ExamplePlugin extends JavaPlugin {
+public class EmberArcane extends JavaPlugin {
     private static final HytaleLogger LOGGER = HytaleLogger.forEnclosingClass();
 
-    public ExamplePlugin(JavaPluginInit init) {
+    public EmberArcane(JavaPluginInit init) {
         super(init);
         LOGGER.atInfo().log("Hello from %s version %s", this.getName(), this.getManifest().getVersion().toString());
     }
--- src/main/java/org/polli/emberarcane/ExampleCommand.java
+++ src/main/java/org/polli/emberarcane/ExampleCommand.java
@@ -1,4 +1,4 @@
-package com.example.exampleplugin;
+package org.polli.emberarcane;
 
 import com.hypixel.hytale.protocol.GameMode;
 import com.hypixel.hytale.server.core.Message;
